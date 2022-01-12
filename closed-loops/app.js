4abd52ecd78abdd9ce437da19e204f135f05458f
cherryvue <yetiasg@gmail.com>
2022-01-12 14:48:53 +0100

    feat: closed-loops

---
+++ closed-loops/app.js
@@ -0,0 +1,11 @@
+const countClosedLoops = number => {
+  let loops = 0
+  let numbers = number.toString().split('')
+  numbers.forEach(num => {
+    if( num === '6' || num === '9') loops++
+    if( num === '8' ) loops+=2
+  })
+  return loops
+}
+
+console.log(countClosedLoops(6886))
