a93343a4a0fa5407f2440bb5f88810eb163ce724
cherryvue <yetiasg@gmail.com>
2022-01-13 09:45:02 +0100

    fix: array-pair-sum

--- array-pair-sum/app.js
+++ array-pair-sum/app.js
@@ -1,16 +1,15 @@
 const findPairs = (sum, numbers) => {
-  const pairs = []
+  const map = {}
+  const results = []
   numbers.forEach((num, i) => {
-    if(numbers.includes(sum-num)){
-      if(numbers.indexOf(sum-num) !== numbers.indexOf(num)){
-        pairs.push({num1: sum-num, num2: num})
-        numbers[numbers.indexOf(sum-num)]=null
-        numbers[i]=null
-      }
+    if(map[num]) {
+      results.push([map[num], num]);
+      map[sum - num] = null
     }
+    else map[sum - num] = num
   })
-  return pairs
+  return results
 }
 
 console.log(findPairs(10, [3, 4, 5, 5, 6, 7]))
-// console.log(findPairs(8, [3, 4, 5, 4, 4]))
+console.log(findPairs(8, [3, 4, 5, 4, 4, 3, 2]))
