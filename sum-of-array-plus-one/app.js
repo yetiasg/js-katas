8ade9c26f249067a299d36911c4ff821f04ee9fb
cherryvue <yetiasg@gmail.com>
2022-02-02 07:55:19 +0100

    Update sum-of-arr-plus-one

--- sum-of-array-plus-one/app.js
+++ sum-of-array-plus-one/app.js
@@ -1,3 +1,7 @@
-exports.sumPlusOne = (arr) => {
-  return arr.reduce((prev, curr) => prev + curr, arr.length)
+exports.sumPlusOne = (arr = []) => {
+  if(Array.isArray(arr)) {
+    if(arr.some(el => typeof el !== 'number')) return 'You can sum numbers only'
+    else return  arr.reduce((prev, curr) => prev + curr, arr.length)
+  } 
+  return 'You need to pass numbers in array'
 };
--- sum-of-array-plus-one/app.spec.js
+++ sum-of-array-plus-one/app.spec.js
@@ -3,8 +3,13 @@ const { sumPlusOne } = require('./app.js')
 
 describe('Test summing all elements in array with addition of 1 to each element', () => {
   it('sums all elements with addition of 1 to each element', () => {
-    const numbers = [1, 2, 3, 4]    //10 + 4 (elements)
-    expect(sumPlusOne(numbers)).toEqual(14)
-    expect(sumPlusOne(numbers)).not.toEqual(10)
+    expect(sumPlusOne([1, 2, 3, 4])).toEqual(14)
+    expect(sumPlusOne([3.5, 4.5])).toEqual(10)
+    expect(sumPlusOne([2])).toEqual(3)
+    expect(sumPlusOne([])).toEqual(0)
+    expect(sumPlusOne([1, 2, '3'])).toEqual('You can sum numbers only')
+    expect(sumPlusOne(5)).toEqual('You need to pass numbers in array')
+    expect(sumPlusOne('h')).toEqual('You need to pass numbers in array')
+    expect(sumPlusOne([true])).toEqual('You can sum numbers only')
   })
 })
